4dc186e903b52ce7723b0f5f00693ab96aa79e10
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-27 19:38:35 +0700

    menambahkan DestinasiHome untuk rute halaman utama

---
+++ app/src/main/java/com/example/prak7/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.prak7.view.route
+
+import com.example.prak7.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+
+    override val titleRes = R.string.app_name
+}
