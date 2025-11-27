4b225f1b0eb58486c74cdf174016a2f0d75765c5
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-27 21:09:33 +0700

    Memperbaiki dan menambahkan AndroidManifest, DestinasiEntry, dan PenyediaViewModel

--- app/src/main/java/com/example/prak7/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/prak7/view/route/DestinasiEntry.kt
@@ -1,6 +1,8 @@
 package com.example.prak7.view.route
 
 import com.example.prak7.R
+import com.example.prak7.view.route.DestinasiNavigasi
+
 
 object DestinasiEntry :DestinasiNavigasi {
     override val route = "item_entry"
--- app/src/main/java/com/example/prak7/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak7/viewmodel/provider/PenyediaViewModel.kt
@@ -1,18 +1,31 @@
 package com.example.prak7.viewmodel.provider
 
+
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.prak7.repositori.AplikasiSiswa
 import com.example.prak7.viewmodel.EntryViewModel
 import com.example.prak7.viewmodel.HomeViewModel
-import com.example.prak7.repositori.AplikasiSiswa
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+
         initializer {
-            HomeViewModel(AplikasiSiswa().container.repositoriSiswa)
+            val aplikasiSiswa = this.aplikasiSiswa()
+            HomeViewModel(
+                aplikasiSiswa.container.repositoriSiswa
+            )
         }
         initializer {
-            EntryViewModel(AplikasiSiswa().container.repositoriSiswa)
+            // Ulangi untuk EntryViewModel
+            val aplikasiSiswa = this.aplikasiSiswa()
+            EntryViewModel(
+                aplikasiSiswa.container.repositoriSiswa
+            )
         }
     }
 }
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
