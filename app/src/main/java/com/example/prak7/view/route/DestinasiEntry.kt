b01dc779f7878625feee4e60019486aa50614011
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-27 19:37:04 +0700

    menambahkan DestinasiEntry untuk rute entry siswa

---
+++ app/src/main/java/com/example/prak7/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.prak7.view.route
+
+import com.example.prak7.R
+
+object DestinasiEntry :DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
