525d402d95bf71e8970282d3c7c1fc8f19e61b3a
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-12-04 15:20:01 +0700

    Menambahkan ViewModel untuk detail siswa dengan SavedStateHandle dan RepositoriSiswa

---
+++ app/src/main/java/com/example/prak7/viewmodel/DetailViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.prak7.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.prak7.repositori.RepositoriSiswa
+
+class DetailViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel(){
+
+}
+
